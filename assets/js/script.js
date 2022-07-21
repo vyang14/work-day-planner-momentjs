0c3a5134385c40ffec862405c2335138ed956660
vyang14 <vyang1014888@gmail.com>
2022-07-21 01:05:18 -0400

    items storing to local storage. grabbing items from local storage still broken

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-var theHours = ['12AM', '1AM','2AM','3AM','4AM','5AM','6AM','7AM','8AM', '9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM', '8PM', '9PM', '10PM', '11PM']
+var theHours = ['12AM', '1AM','2AM','3AM','4AM','5AM','6AM','7AM','8AM', '9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM','6PM','7PM', '8PM', '9PM', '10PM', '11PM'];
+var toStorage = new Array(24);
 var planEl = document.getElementById('timeBlocks');
 var todayMyDudes = moment().format('ddd MMM Do, YYYY');
 $("#currentDay").text(todayMyDudes);
@@ -9,30 +10,45 @@ function theTimeIsNow(){
 };
 
 function saveContent(){
-    var storedValues = {
+    var fromStorage = JSON.parse([localStorage.getItem("workDay")]);
+    var taskNum = parseInt(this.previousElementSibling.classList[2]);
+    var saveTask =  {
     time: this.previousElementSibling.previousElementSibling.textContent,
     task: this.previousElementSibling.value
     };
-    localStorage.setItem("storedStuff", JSON.stringify(storedValues));
+    toStorage = fromStorage;
+    toStorage[taskNum] = saveTask;
+    localStorage.setItem("workDay", JSON.stringify(toStorage));
 };
+
+function getTask(i){
+    var fromStorage = JSON.parse([localStorage.getItem("workDay")]);
+
+    if (fromStorage[i] != undefined) {
+        taskRow.innerHTML = fromStorage[i].task;
+    } else {
+        taskRow.innerHTML = '';
+    }
+    return;
+}
 
 setInterval(theTimeIsNow, 1000);
 
 for (var i = 8; i < 19; i++){
-    console.log(i + ' = i');
+    var stored = localStorage.getItem("workDay");
     var entireRow = document.createElement('div');
     var timeEl = document.createElement('div');
     var hourEl = document.createElement('div');
     var taskRow = document.createElement('textarea');
     var saveRow = document.createElement('button');
 
-    hourEl.appendChild(document.createTextNode(theHours[i]));
-    saveRow.innerHTML = '💾';
     entireRow.classList.add('row');
     timeEl.classList.add('timeOfDay', 'col-2');
-    taskRow.classList.add('input','col-9', `task${i}`);
+    taskRow.classList.add('input','col-9', i);
     saveRow.classList.add('save','col-1');
 
+    hourEl.appendChild(document.createTextNode(theHours[i]));
+    
     if (i < moment().format('HH')) {
         taskRow.classList.add('past');
     } else if (i === moment().format('HH')){
@@ -40,7 +56,26 @@ for (var i = 8; i < 19; i++){
     } else {
         taskRow.classList.add('future');
     }
-    
+
+    if (stored = null){
+        taskRow.innerHTML = '';
+        break;
+    } else {    
+        var fromStorage = [localStorage.getItem("workDay")];
+        if (fromStorage[i] === null) {
+            taskRow.innerHTML = '';
+            break;
+        } else {
+        if (fromStorage[i].time = hourEl.innerHTML) {
+        taskRow.innerHTML = fromStorage[i].task;
+        } else {
+        taskRow.innerHTML = '';
+        }
+    }
+    }
+debugger;
+    saveRow.innerHTML = '💾';
+
     timeEl.appendChild(hourEl);
     entireRow.appendChild(timeEl);
     entireRow.appendChild(taskRow);
@@ -49,4 +84,3 @@ for (var i = 8; i < 19; i++){
 
     saveRow.addEventListener('click', saveContent);
 };
-
